fd9b4f589fa077cf8fcaa5e889d5d386f3a43c90
Pingumania <pingumania@users.noreply.github.com>
2023-04-12 11:21:59 +0200

    Fix short names

--- custom.name-short.lua
+++ custom.name-short.lua
@@ -6,9 +6,15 @@ local mod = addon:NewPlugin("Custom_NameShort", 101)
 if not mod then return end
 
 function mod:Show(f)
-    f.NameText:SetText(gsub(f.state.name, "(%S+) ", function(t) return t:sub(1, 1) .. "." end))
+    if f.IN_NAMEONLY then return end
+    if f.state then
+        f.NameText:SetText(gsub(f.state.name, "(%S+) ", function(t) return t:sub(1, 1) .. "." end))
+    end
 end
 
 function mod:Enable()
+    for _, f in addon:Frames() do
+        self:Show(f)
+    end
     self:RegisterMessage("Show")
 end
