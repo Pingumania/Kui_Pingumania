92f5f93e8ce1b811b50262084a5f55264ec10026
Pingumania <pingumania@users.noreply.github.com>
2023-04-12 11:18:13 +0200

    Make nameplates pixel perfect

---
+++ custom.pixelperfect.lua
@@ -0,0 +1,29 @@
+local _, ns = ...
+if not ns.modules.name_short then return end
+
+local addon = KuiNameplates
+local mod = addon:NewPlugin("Custom_PixelPerfect", 101)
+if not mod then return end
+
+local function SetScale(f)
+    f:SetIgnoreParentScale(true)
+    f:SetScale(PixelUtil.GetPixelToUIUnitFactor())
+end
+
+function mod:Show(f)
+    SetScale(f)
+end
+
+function mod:Create(f)
+    SetScale(f)
+end
+
+function mod:OnEnable()
+    for _, f in addon:Frames() do
+        SetScale(f)
+    end
+    self:RegisterMessage("Create")
+    self:RegisterMessage("Show")
+
+    addon.UI_SCALE_CHANGED = function() end
+end
