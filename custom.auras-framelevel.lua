e5df5ddbb3f427e9cc8375b81ba191fecdf43301
Pingumania <pingumania@users.noreply.github.com>
2023-04-12 11:19:18 +0200

    Make sure auras are behind the name

---
+++ custom.auras-framelevel.lua
@@ -0,0 +1,21 @@
+local _, ns = ...
+if not ns.modules.name_short then return end
+
+local addon = KuiNameplates
+local mod = addon:NewPlugin("Custom_AurasFrameLevel", 101)
+if not mod then return end
+
+function mod:Create(f)
+    if f.IN_NAMEONLY then return end
+    f:SetFrameLevel(1)
+    for _, frame in pairs(f.Auras.frames) do
+        frame:SetFrameLevel(f:GetFrameLevel() - 1)
+    end
+end
+
+function mod:Enable()
+    for _, f in addon:Frames() do
+        self:Create(f)
+    end
+    self:RegisterMessage("Create")
+end
